2c7de87ba63e9a6159acc684937a30de62e4df2a
iamNanak <iamnanak01@gmail.com>
2026-04-26 00:52:39 +0530

    added the logic of creating new file and saving it

--- main.go
+++ main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/charmbracelet/bubbles/textarea"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
+var (
+	vaultDir    string
+	cursorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+)
+
+func init() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal("Error in geting home directory", err)
+	}
+	vaultDir = fmt.Sprintf("%s/.gopad", homeDir)
+}
+
 type model struct {
 	newFileInput           textinput.Model
+	currentFile            *os.File
 	createFileInputVisible bool
+	noteTextArea           textarea.Model
 }
 
 func (m model) Init() tea.Cmd {
@@ -30,7 +47,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 
 		// These keys should exit the program.
-		case "ctrl+c", "q":
+		case "ctrl+c":
 			fmt.Println("Pressed key:", msg.String())
 			return m, tea.Quit
 
@@ -39,11 +56,71 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.createFileInputVisible = true
 
 			return m, nil
+
+		case "enter":
+			if m.createFileInputVisible {
+				filename := m.newFileInput.Value()
+				if filename != "" {
+					filePath := fmt.Sprintf("%s/%s.txt", vaultDir, filename)
+					fileInfo, err := os.Stat(filePath)
+					if err == nil && !fileInfo.IsDir() {
+						fmt.Printf("File '%s' already exists!\n", filename)
+						return m, nil
+					} else {
+						file, err := os.Create(filePath)
+
+						if err != nil {
+							log.Printf("Error creating file: %v\n", err)
+						} else {
+							fmt.Printf("File '%s' created successfully!\n", filename)
+							m.currentFile = file
+						}
+					}
+
+				}
+				m.createFileInputVisible = false
+				m.newFileInput.SetValue("") // Clear the input field after creating the file
+
+			}
+			return m, nil
+
+		case "ctrl+s":
+			if m.currentFile == nil {
+				break
+			}
+
+			if err := m.currentFile.Truncate(0); err != nil {
+				log.Printf("Error truncating file: %v\n", err)
+				return m, nil
+			}
+
+			if _, err := m.currentFile.Seek(0, 0); err != nil {
+				log.Printf("Error seeking file: %v\n", err)
+				return m, nil
+			}
+
+			if m.currentFile != nil {
+				content := m.noteTextArea.Value()
+				_, err := m.currentFile.WriteString(content)
+				if err != nil {
+					log.Printf("Error saving file: %v\n", err)
+				} else {
+					fmt.Printf("File '%s' saved successfully!\n", m.currentFile.Name())
+				}
+				m.currentFile.Close()
+				m.currentFile = nil
+				m.noteTextArea.SetValue("") // Clear the text area after saving the file
+			}
 		}
 	}
 
 	if m.createFileInputVisible {
 		m.newFileInput, cmd = m.newFileInput.Update(msg)
+		return m, cmd
+	}
+
+	if m.currentFile != nil {
+		m.noteTextArea, cmd = m.noteTextArea.Update(msg)
 		return m, cmd
 	}
 
@@ -60,7 +137,7 @@ func (m model) View() string {
 		Background(lipgloss.Color("205")).Padding(0, 1, 0, 1)
 
 	text := style.Render("Weclcome to GoPad! ")
-	helpKeys := "Ctrl+N : New File | Ctrl+S : Save File | Ctrl+O : Open File | Ctrl+L : List Files | Ctrl+C or q : Quit"
+	helpKeys := "Ctrl+N : New File | Ctrl+S : Save File | Ctrl+O : Open File | Ctrl+L : List Files | Ctrl+C : Quit"
 	note := "Use the following keybindings to navigate and manage your files:"
 
 	view := ""
@@ -68,20 +145,42 @@ func (m model) View() string {
 		view += m.newFileInput.View()
 	}
 
+	if m.currentFile != nil {
+
+		view = m.noteTextArea.View()
+	}
+
 	return fmt.Sprintf("\n%s\n\n%s\n\n%s\n%s", text, view, note, helpKeys)
 }
 
 func initialModel() model {
+
+	err := os.MkdirAll(vaultDir, 0750)
+	if err != nil {
+		log.Fatalf("Error creating vault directory: %v\n", err)
+		os.Exit(1)
+	}
+
 	// initialize the text input component
 	ti := textinput.New()
 	ti.Placeholder = "What would you like to call your file?"
 	ti.Focus()
 	ti.CharLimit = 156
 	ti.Width = 50
+	ti.Cursor.Style = cursorStyle
+
+	// initialize the text area component
+	ta := textarea.New()
+	ta.Placeholder = "Start writing your notes here..."
+	ta.Focus()
+	ta.CharLimit = 10000
+	ta.ShowLineNumbers = false
+	ta.Cursor.Style = cursorStyle
 
 	return model{
 		newFileInput:           ti,
-		createFileInputVisible: false}
+		createFileInputVisible: false,
+		noteTextArea:           ta}
 }
 
 func main() {
